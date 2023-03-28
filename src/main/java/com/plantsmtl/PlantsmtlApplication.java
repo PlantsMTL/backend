0b943dd50e02215c2f984b88b6a08fa07ea272ad
Mario Camelo <mariuo@gmail.com>
2023-03-28 19:00:48 -0400

    :sparkles: feat: Maven project inited

---
+++ src/main/java/com/plantsmtl/PlantsmtlApplication.java
@@ -0,0 +1,13 @@
+package com.plantsmtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantsmtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantsmtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantsmtl/PlantsmtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantsmtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsmtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
